6971b1b7635deba71c28fe26483badd61aa5aa97
Ra-Ni <32001054+Ra-Ni@users.noreply.github.com>
2021-11-22 21:44:37 -0500

    setup testing

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="Conupedia",
+    version="1.0.0",
+    author="Rani Rafid",
+    author_email="rani.rafid@concordia.ca",
+    description="Currently in development",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Ra-Ni/Conupedia",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
