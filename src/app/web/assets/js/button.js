39082e2949e1454526ce335832f92f7175d6629e
Ra-Ni <32001054+Ra-Ni@users.noreply.github.com>
2021-11-28 16:42:41 -0500

    signup and login changed

--- src/app/web/assets/js/button.js
+++ src/app/web/assets/js/button.js
@@ -2,3 +2,7 @@ document.getElementById("signup").onclick = function () {
     window.location.replace('http://www.securesea.ca/signup');
 };
 
+document.getElementById("login").onclick = function () {
+    window.location.replace('http://www.securesea.ca/login');
+};
+
